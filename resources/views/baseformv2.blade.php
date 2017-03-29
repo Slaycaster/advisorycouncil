e2bef042ba87a0acad15ca1c3f350164cecac48d
tineamper <christinejoyceamper13@gmail.com>
2017-03-29 20:23:58 +0800

    rwdres again

--- resources/views/baseformv2.blade.php
+++ resources/views/baseformv2.blade.php
@@ -15,11 +15,7 @@
 		<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/icon.css')}}">
 		<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/res.css')}}">
 
-		<link rel="stylesheet" type="text/css" media="only screen and (max-width: 767px) , only screen and (max-device-width: 767px)" href="{{ URL::asset('css/mobileDevice.css')}}" />
-		<link rel="stylesheet" type="text/css" media="only screen and (min-width: 768px) and (max-width: 991px), only screen and  (min-device-width: 768px) and (max-device-width: 991px)" href="{{ URL::asset('css/tabletDevice.css')}}" />
-		<link rel="stylesheet" type="text/css" media="only screen and (min-width: 992px) and (max-width: 1199px), only screen and (min-device-width: 992px) and (max-device-width: 1199px)" href="{{ URL::asset('css/computerMonitor.css')}}" />
-		<link rel="stylesheet" type="text/css" media="only screen and (max-width: 1920px) , only screen and (max-device-width: 1920px)" href="{{ URL::asset('css/wideScreenMonitor.css')}}" />
-		<link rel="stylesheet" type="text/css" media="only screen and (min-width: 1200px) and (max-width: 1919px) , only screen and and (min-device-width: 1200px) and (max-device-width: 1919px)" href="{{ URL::asset('css/largeMonitor.css')}}" />
+	
 
 
 		<!-- JS -->
